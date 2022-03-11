de3d8cf8511ac39a0eb41071975ba6dd3d8e6903
igorrebeche <igorrebeche@gmail.com>
2022-03-11 00:21:37 -0300

    feat: add rabbitmq + masstransit integration

--- Application/Tasks/ProcessTickerTask.cs
+++ Application/Tasks/ProcessTickerTask.cs
@@ -1,28 +1,47 @@
 ﻿using Application.Services.BinanceService;
 using Application.UseCases.CollectTickersUseCase;
+using Events;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Infrastructure.Tasks
+namespace Application.Tasks
 {
     public class ProcessTickerTask : IHostedService
     {
         private readonly ILogger<ProcessTickerTask> _logger;
         private readonly ICollectTickersUseCase _collectTickersUseCase;
         private readonly IBinanceService _binanceService;
+        private readonly IPublishEndpoint _publishEndpoint;
 
-        public ProcessTickerTask(ILogger<ProcessTickerTask> logger, IBinanceService binanceService)
+        public ProcessTickerTask(ILogger<ProcessTickerTask> logger, IBinanceService binanceService, IServiceScopeFactory factory)
         {
             _logger = logger;
             _binanceService = binanceService;
+            _publishEndpoint = factory.CreateScope().ServiceProvider.GetRequiredService<IPublishEndpoint>();
         }
         public Task StartAsync(CancellationToken cancellationToken)
         {
             _logger.LogInformation("Start Job");
             List<string> pairs = new List<string>();
             pairs.Add("btcusdt");
-            pairs.Add("ethusdt");
-            Task.Run(async () => _binanceService.GetBookTickerStreams(pairs, msg => _logger.LogInformation($"Message received from UseCase: {msg}")));
+            //pairs.Add("ethusdt");
+            Task.Run(async () =>
+            {
+                await _binanceService.GetBookTickerStreams(pairs, async msg =>
+                {
+                    _logger.LogInformation($"Message received from UseCase: {msg}");
+                    await _publishEndpoint.Publish<ITickerCollected>(new
+                    {
+                        Price = msg,
+                        TimeStamp = DateTime.Now
+                    });
+
+                });
+            });
+
+
             return Task.CompletedTask;
         }
 
--- market-price-binance-collector-service/Program.cs
+++ market-price-binance-collector-service/Program.cs
@@ -1,11 +1,12 @@
 
 
+using Application.Consumers;
 using Application.Extensions;
-using Application.Services.BinanceService;
-using Application.UseCases.CollectTickersUseCase;
+using Application.Tasks;
 using Infrastructure;
-using Infrastructure.Services.BinanceService;
-using Infrastructure.Tasks;
+using MassTransit;
+using Serilog;
+using Serilog.Events;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,11 +16,44 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication(builder.Configuration);
 builder.Services.AddHostedService<ProcessTickerTask>();
 
+// Add Masstransit
+builder.Services.AddMassTransit(x =>
+{
+    //x.AddConsumer<TickerCollectedConsumer>();
+    x.UsingRabbitMq( (context, cfg) =>
+    {
+        cfg.Host("localhost", "/", h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+        //cfg.ReceiveEndpoint("Events:ITickerCollected", e =>
+        //{
+        //    e.Consumer<TickerCollectedConsumer>(context);
+        //});
+    });
+});
+builder.Services.AddMassTransitHostedService();
+
+builder.Host.UseSerilog((host, log) =>
+{
+    if (host.HostingEnvironment.IsProduction())
+        log.MinimumLevel.Information();
+    else
+        log.MinimumLevel.Debug();
+
+    log.MinimumLevel.Override("Microsoft", LogEventLevel.Warning);
+    log.MinimumLevel.Override("Quartz", LogEventLevel.Information);
+    log.WriteTo.Console();
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
